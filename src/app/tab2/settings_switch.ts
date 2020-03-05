aaf40f00885d4e71dec7b7c2812009e87a673dc8
Andreas Due Pihl <6140251@RTS-365.dk>
2020-03-05 15:05:26 +0100

    settings continued (arbejder på local storage imorgen)

--- src/app/tab2/settings_switch.ts
+++ src/app/tab2/settings_switch.ts
@@ -1,36 +1,25 @@
+export function execute() {
 
-var settingchange = document.querySelector(".individual__setting")
-
-
-
+    let settingchange = document.querySelector(".individual__setting")
 
 
-function change() {
+
+    localStorage.setItem("1", ".white__bg");
+
+
+
+    settingchange.addEventListener("click", change)
+
+
+    function change(e) {
+
+
+        e.target.classList.toggle("white__bg");
+        console.log(localStorage)
+    }
+
+
 
 
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-export default change
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,16 +1,18 @@
-import { Component } from '@angular/core';
-import change from './settings_switch'
+import { Component, OnInit } from '@angular/core';
+
+
+import * as settingsSwitchScript from './settings_switch'
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
 
   constructor() { }
 
+  ngOnInit() {
+    settingsSwitchScript.execute();
+  }
 }
-
-change()
-
