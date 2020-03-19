88fb4f8074e6c7afeb60bbbf446b78c380eebeed
Andreas Due Pihl <6140251@RTS-365.dk>
2020-03-19 12:29:35 +0100

    ...

--- src/app/tab2/settings_switch.ts
+++ src/app/tab2/settings_switch.ts
@@ -1,10 +1,22 @@
+import { puts } from 'util';
+
 export function execute() {
 
     let settingchange = document.querySelector(".individual__setting")
 
 
 
-    localStorage.setItem("1", ".white__bg");
+
+
+
+
+    var currentTheme = localStorage.getItem("currentTheme");
+    document.documentElement.setAttribute("data-theme", currentTheme);
+
+
+
+
+
 
 
 
@@ -16,8 +28,35 @@ export function execute() {
 
         e.target.classList.toggle("white__bg");
         console.log(localStorage)
+
+        fetch("http://192.168.8.100/api/4lIQDtPyXMDt9Zjfnjf4FcFffWTRYRg9U71eWuP0/lights/6/state", {
+            method: "PUT",
+            body: JSON.stringify({ on: true, sat: 254, bri: 154, hue: 10000 })
+        })
+            .then(Response => Response.json())
+            .then(light => {
+                console.log(light)
+            })
+
+
+
+
+        if (localStorage.getItem("1") === ".white__bg") {
+            localStorage.setItem("1", ".individual__setting_Recess")
+        }
+        else {
+            localStorage.setItem("1", ".white__bg");
+        }
+
     }
 
+    const list = document.querySelectorAll(".individual__setting p")
+    console.log(list)
+
+
+
+
+    // Light variables
 
 
 
